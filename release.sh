6a75f6c6fe3eec3d251496740a4d088333281f0b
JadKHaddad <jadkhaddad@gmail.com>
2023-09-21 20:23:27 +0200

    Added a release helper
    Signed-off-by: JadKHaddad <jadkhaddad@gmail.com>

---
+++ release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set version in cargo.toml
+
+# Build and push docker image
+# Since our package knows excactly how to build itself and manages its own versioning:
+cargo run -- docker up 
+
+# Create a ready to use crd.yaml
+cargo run -- crd write -f crd.yaml
+
+# Create a ready to use operator manifest
+cargo run -- operator controller deploy write -f operator.yaml
